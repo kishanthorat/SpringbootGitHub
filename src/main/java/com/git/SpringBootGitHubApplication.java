e9995c495459f13857efd0e99b074d92efd1f0b1
Kishan Thorat <kishanthorat5@gmail.com>
2023-05-09 23:30:44 +0530

    TL commit

--- src/main/java/com/git/SpringBootGitHubApplication.java
+++ src/main/java/com/git/SpringBootGitHubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGitHubApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application Start...");
 		SpringApplication.run(SpringBootGitHubApplication.class, args);
 	}
 
