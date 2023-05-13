fc14ed4720de6efa4a720edacd53b6d2a1f41ee6
Kishan Thorat <kishanthorat5@gmail.com>
2023-05-13 19:51:09 +0530

    pojo created

--- src/main/java/com/git/model/Employee.java
+++ src/main/java/com/git/model/Employee.java
@@ -1,5 +1,19 @@
 package com.git.model;
 
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+	 
+	private Integer id;
+	private String name;
+	private Double salary;
 
 }
