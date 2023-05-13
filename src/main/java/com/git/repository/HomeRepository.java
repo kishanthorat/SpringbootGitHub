bddf9367127550c492db78fffff8707dc408f152
Kishan Thorat <kishanthorat5@gmail.com>
2023-05-13 22:31:13 +0530

    push to the branch-x

--- src/main/java/com/git/repository/HomeRepository.java
+++ src/main/java/com/git/repository/HomeRepository.java
@@ -1,5 +1,11 @@
 package com.git.repository;
 
-public interface HomeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.git.model.Employee;
+
+@Repository
+public interface HomeRepository extends JpaRepository<Employee, Integer>{
 
 }
